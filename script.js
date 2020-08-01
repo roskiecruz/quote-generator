2fc9280bf3fd5bd116d478c7f17e68283b387a6e
roskiecruz <roskiecruz@gmail.com>
2020-08-01 16:35:41 -0700

    Implemented loader

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const quoteContainer = document.getElementById('quote__container');
+const quoteContainer = document.getElementById('quote');
 const quoteText= document.getElementById('quote-content');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+const errorLimit = 0;
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete(){
+    if (!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get quote from Forismatic API
 async function getQuote(){
+    loading();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(proxyUrl + apiUrl);
+        const response = await fetch(`${proxyUrl}${apiUrl}`, {
+            method: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            }
+        });
         const data = await response.json();
         // If author is blank, add 'Unknown' placeholder
         if(data.quoteAuthor === ''){
@@ -24,10 +48,29 @@ async function getQuote(){
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // Stop loader, show quote
+        complete();
     } catch (error) {
-        getQuote();
+        if(errorLimit <= 3){
+            errorLimit++;
+            getQuote();
+        } else {
+            console.log('Uhhhh... ', error);
+        }
     }
 }
+
+// Tweet quote
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners - any time buttons are clicked, functions are run
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuote();
