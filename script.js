9ed0732fc0285a90ebb369472f9474db8fd9ee20
roskiecruz <roskiecruz@gmail.com>
2020-08-01 15:40:44 -0700

    Implemented quick fix for CORS issue, will implement own CORS fix later

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Get quote from Forismatic API
 async function getQuote(){
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         console.log(data);
     } catch (error) {
+        getQuote();
         console.log('Whoops, no quote', error);
     }
 }
